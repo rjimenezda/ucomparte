f8cf890dd222bdbf3502f90900190bad2caad8fd
Carlos Garcia <ciyinet@gmail.com>
2011-12-23 19:56:21 +0100

    #4 Actualizacion de API

---
+++ api/getgroupdetails.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+
+include("../www/dataconnection.php");
+
+if (!isset($_POST['grupo_id'])) {
+	header('HTTP/1.1 500 Internal Server Error');
+	mysql_close($conexion);
+	die();
+}
+
+else {
+	$queEmp = "SELECT * FROM grupo WHERE grupo_id="."$_POST['grupo_id']";
+	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+	$totEmp = mysql_num_rows($resEmp);
+
+
+	if ($totEmp> 0) {
+	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
+			// Imprimimos los resultados en JSON
+			echo json_encode($rowEmp);
+
+		//	echo "<strong>".$rowEmp['Recurso_id']."</strong><br>";
+		// 	echo "Direccion: ".$rowEmp['Contenido']."<br>";
+	   }
+	}
+}
+
+mysql_close($conexion);
+?>
---
+++ api/getgrouppost.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+
+include("../www/dataconnection.php");
+
+if (!isset($_POST['publicacion_id'])) {
+	header('HTTP/1.1 500 Internal Server Error');
+	mysql_close($conexion);
+	die();
+}
+
+else {
+	$queEmp = "SELECT * FROM respuesta WHERE publicacion_id="."$_POST['publicacion_id']";
+	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+	$totEmp = mysql_num_rows($resEmp);
+
+
+	if ($totEmp> 0) {
+	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
+			// Imprimimos los resultados en JSON
+			echo json_encode($rowEmp);
+
+		//	echo "<strong>".$rowEmp['Recurso_id']."</strong><br>";
+		// 	echo "Direccion: ".$rowEmp['Contenido']."<br>";
+	   }
+	}
+}
+
+mysql_close($conexion);
+?>
