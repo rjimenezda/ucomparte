d5f159916a0543ace2b76da7875dee6d1a970bde
Carlos Garcia <ciyinet@gmail.com>
2011-12-23 19:55:31 +0100

    #4 Actualizacion de API

--- api/getuserinfo.php
+++ api/getuserinfo.php
@@ -2,26 +2,30 @@
 
 session_start();
 
-include("../www/dataconection.php");
+include("../www/dataconnection.php");
 
-$queEmp = "SELECT * FROM reporte";
-$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
-$totEmp = mysql_num_rows($resEmp);
-
-
-if ($totEmp> 0) {
-   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
-      echo "<strong>".$rowEmp['Recurso_id']."</strong><br>";
-      echo "Direccion: ".$rowEmp['Contenido']."<br>";
-   }
-}
-
-
-if (!isset($_POST['username'])) {
+if (!isset($_POST['usuario_id'])) {
 	header('HTTP/1.1 500 Internal Server Error');
+	mysql_close($conexion);
 	die();
 }
 
-echo "hola";
+else {
+	$queEmp = "SELECT * FROM usuario WHERE usuario_id="."$_POST['usuario_id']";
+	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+	$totEmp = mysql_num_rows($resEmp);
 
+
+	if ($totEmp> 0) {
+	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
+			// Imprimimos los resultados en JSON
+			echo json_encode($rowEmp);
+
+		//	echo "<strong>".$rowEmp['Recurso_id']."</strong><br>";
+		// 	echo "Direccion: ".$rowEmp['Contenido']."<br>";
+	   }
+	}
+}
+
+mysql_close($conexion);
 ?>
