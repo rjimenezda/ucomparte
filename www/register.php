3fd2ac113d57b8988cdb470666946479613a0233
Román Jiménez <drrouman@gmail.com>
2011-12-23 20:03:02 +0100

    Issue #4: Mejorando el registro @jdexpositocanete Echale un vistazo al diseño que lo he jodido un poco xD

--- www/register.php
+++ www/register.php
@@ -1,19 +1,36 @@
 <?php
 
-session_start();
+include('../www/dataconnection.php');
 
-if (!isset($_POST['name']) or  
-	!isset($_POST['surname']) or
-	!isset($_POST['degree']) or
-	!isset($_POST['town']) or 
-	!isset($_POST['province']) or
-	!isset($_POST['birthdate']) or
-	!isset($_POST['name']) or 
-	!isset($_POST['log']) or
-	!isset($_POST['pwd'])) {
-		die();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+session_start();
+$query = "";
+
+if ($_POST['name'] === '' or  
+	$_POST['surname'] === '' or
+	$_POST['degree'] === '' or
+	$_POST['town'] === '' or 
+	$_POST['province'] === '' or
+	$_POST['birthdate'] === '' or
+	$_POST['name'] === '' or 
+	$_POST['log'] === '' or
+	$_POST['pwd'] === '') {
+		die("Form invalido");
+	} else {
+		
+		if (strlen($_POST['log']) <= 15) {
+			$query = sprintf('INSERT INTO usuario (email, password, es_administrador, nombre, apellidos, pais, localidad, provincia, sexo, fecha_nacimiento, fecha_alta) 
+								values("%s", "%s",%d,"%s","%s", "%s","%s", "%s", "%s", NOW(), NOW())',
+							$_POST['log'], $_POST['pwd'], 0, $_POST['name'], $_POST['surname'], $_POST['degree'], $_POST['town'], $_POST['province'], $_POST['sex']);
+			
+			mysql_query($query);
+			mysql_close($conexion);
+		} else {
+			die("Illegal email");	
+		}
+		
 	}
-	
-	
 
 ?>
