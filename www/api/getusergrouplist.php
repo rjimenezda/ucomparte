87ed717895d0be1216f8fd2665c8605d6fb42fae
Román Jiménez <drrouman@gmail.com>
2011-12-24 14:16:05 +0100

    Issue #4: Añadiendo función que devuelve Id+Nombre de grupos de usuario Pensado para la lista que saldrá a la izquierda

---
+++ www/api/getusergrouplist.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+include("../dataconnection.php");
+
+if (!isset($_POST['usuario_id'])) {
+	header('HTTP/1.1 500 Internal Server Error');
+	mysql_close($conexion);
+	die();
+}
+
+else {
+	$queEmp = sprintf("SELECT grupo_id, nombre FROM grupo WHERE grupo_id IN (SELECT grupo_id FROM grupo_usuario WHERE usuario_id = %d)", $_POST['usuario_id']);
+	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+	$totEmp = mysql_num_rows($resEmp);
+
+	if ($totEmp> 0) {
+	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
+			// Imprimimos los resultados en JSON
+			echo json_encode($rowEmp);
+	   }
+	}
+}
+
+mysql_close($conexion);
+
+?>
