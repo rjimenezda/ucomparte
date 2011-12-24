6fecdd4185e90914fdc93f746927f7514bd17b1b
Román Jiménez <drrouman@gmail.com>
2011-12-24 13:26:31 +0100

    #4 Nueva función del a API para extraer los "Meloapunto"s

---
+++ www/api/getmeloapuntos.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+include("../dataconnection.php");
+
+if (!isset($_POST['usuario_id'])) {
+	header('HTTP/1.1 500 Internal Server Error');
+	mysql_close($conexion);
+	die();
+}
+
+else {
+	$queEmp = "SELECT recurso_id FROM usuario_recurso_apunte WHERE usuario_id=".$_POST['usuario_id'];
+	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+	$totEmp = mysql_num_rows($resEmp);
+
+
+	if ($totEmp> 0) {
+	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
+			// Imprimimos los resultados en JSON
+			echo json_encode($rowEmp);
+	   }
+	}
+}
+
+mysql_close($conexion);
+
+
+?>
