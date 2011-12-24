0624c59b4a1bab94d8a1d609b9fe2b05f5f17d5a
Román Jiménez <drrouman@gmail.com>
2011-12-24 13:22:52 +0100

    #4 Dataconnection necesita que exista la sesión ahora Esto para que no pueda accederse al script y que se abran conexiones por la cara

--- www/dataconnection.php
+++ www/dataconnection.php
@@ -1,5 +1,10 @@
 <?php
 
+// Sin esto los usuarios podrían acceder a esta pantalla y liarnos un buen memory leak xD
+if (!isset($_SESSION)) {
+	die();
+}
+
 $conexion = mysql_connect("localhost", "root", "root");
 
 mysql_select_db("ucomparte", $conexion);
