788a5808ffd4204785f0b268319886ffd89292b9
Carlos Garcia <ciyinet@gmail.com>
2011-12-26 12:09:05 +0100

    #4 Actualizacion de API

---
+++ www/api/creategroup.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+include("../dataconnection.php");
+
+if (!isset($_POST['grupo_id']) or !isset($_POST['nombre']) or !isset($_POST['descripcion'])) {
+	header('HTTP/1.1 500 Internal Server Error');
+	mysql_close($conexion);
+	die();
+}
+
+else {
+	$queEmp = "INSERT INTO grupo VALUES(NULL, '".$_POST['nombre']."', '".$_POST['descripcion']."', NOW())";
+	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+//	$totEmp = mysql_num_rows($resEmp);
+
+
+//	if ($totEmp> 0) {
+//	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
+			// Imprimimos los resultados en JSON
+//			echo json_encode($rowEmp);
+
+		//	echo "<strong>".$rowEmp['Recurso_id']."</strong><br>";
+		// 	echo "Direccion: ".$rowEmp['Contenido']."<br>";
+//	   }
+//	}
+	echo 'OK';
+}
+
+mysql_close($conexion);
+?>
