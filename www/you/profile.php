56f855f5b3c557b36ce66b29699181abc251828b
Román Jiménez <drrouman@gmail.com>
2011-12-26 19:13:32 +0100

    Issue #5: arreglando ausencia de ?php

--- www/you/profile.php
+++ www/you/profile.php
@@ -3,7 +3,7 @@ $(function(){
 	//Se hace la consulta para extraer el nombre completo
 	$.post("../api/getuserinfo.php", { usuario_id : <?php echo $_SESSION['usuario_id']; ?> }, function(data) { $("#nombreCompleto").text( data.nombre + " " +  data.apellidos ); console.log(data)}, "json" )
 
-	$.post("../api/getusergrouplist.php", { usuario_id : <? echo $_SESSION['usuario_id']; ?> }, fillusergroups, "json" )
+	$.post("../api/getusergrouplist.php", { usuario_id : <?php echo $_SESSION['usuario_id']; ?> }, fillusergroups, "json" )
 
 	function fillusergroups(data){
 		$("#group_list_placeholder").empty();
