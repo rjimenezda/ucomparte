59c7dd2f0e757528f116a54184b955e5c59612e0
Román Jiménez <drrouman@gmail.com>
2011-12-28 13:46:18 +0100

    Issue #4: usando terminate() en meloapunto/melodesapunto

--- www/api/meloapunto.php
+++ www/api/meloapunto.php
@@ -2,24 +2,20 @@
 
 //Comprobacion de permisos del usuario
 include("../checkauth.php");
-
+include("../funciones.php");
 include("../dataconnection.php");
 
 if (!isset($_POST['recurso_id']) or
 	$_POST['recurso_id'] == '' or 
 	$_POST['recurso_id'] < 0 or
 	!is_numeric($_POST['recurso_id'])) {
-	header('HTTP/1.1 500 Internal Server Error');
-	mysql_close($conexion);
-	die();
+	terminate($conexion, true, "Bad Parameters");
 }
 
 else {
 	$queEmp = "INSERT INTO usuario_recurso_apunte VALUES(".$_SESSION['usuario_id'].", ".$_POST['recurso_id'].")";
-	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
-
-	echo 'OK';
+	$resEmp = mysql_query($queEmp, $conexion) or terminate($conexion, true, mysql_error());
 }
 
-mysql_close($conexion);
+terminate($conexion);
 ?>
--- www/api/melodesapunto.php
+++ www/api/melodesapunto.php
@@ -2,24 +2,20 @@
 
 //Comprobacion de permisos del usuario
 include("../checkauth.php");
-
+include("../funciones.php");
 include("../dataconnection.php");
 
 if (!isset($_POST['recurso_id']) or
 	$_POST['recurso_id'] == '' or 
 	$_POST['recurso_id'] < 0 or
 	!is_numeric($_POST['recurso_id'])) {
-	header('HTTP/1.1 500 Internal Server Error');
-	mysql_close($conexion);
-	die();
+	terminate($conexion, true, "Bad Parameters");
 }
 
 else {
 	$queEmp = "DELETE FROM usuario_recurso_apunte WHERE usuario_id=".$_SESSION['usuario_id']." AND recurso_id=".$_POST['recurso_id'];
-	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
-	
-	echo 'OK';
+	$resEmp = mysql_query($queEmp, $conexion) or terminate($conexion, true, mysql_error());
 }
 
-mysql_close($conexion);
+terminate($conexion);
 ?>
