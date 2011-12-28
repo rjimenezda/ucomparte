e83c1e44d7de3e24afef57b01900d13fb3904e4d
Román Jiménez <drrouman@gmail.com>
2011-12-28 12:59:06 +0100

    Issue #4: mejorando función API de creación de grupo Ahora devuelve el ID del grupo que se ha creado y además mete al creador en el grupo automáticamente

--- www/api/creategroup.php
+++ www/api/creategroup.php
@@ -2,32 +2,28 @@
 
 //Comprobacion de permisos del usuario
 include("../checkauth.php");
-
+include("../funciones.php");
 include("../dataconnection.php");
 
-if (!isset($_POST['grupo_id']) or !isset($_POST['nombre']) or !isset($_POST['descripcion'])) {
-	header('HTTP/1.1 500 Internal Server Error');
-	mysql_close($conexion);
-	die();
+if (!isset($_POST['nombre']) or 
+	!isset($_POST['descripcion']) or
+	$_POST['nombre'] == "") {
+	terminate($conexion, true, "Bad parameters");
 }
 
 else {
 	$queEmp = "INSERT INTO grupo VALUES(NULL, '".$_POST['nombre']."', '".$_POST['descripcion']."', NOW())";
-	$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
-//	$totEmp = mysql_num_rows($resEmp);
-
-
-//	if ($totEmp> 0) {
-//	   while ($rowEmp = mysql_fetch_assoc($resEmp)) {
-			// Imprimimos los resultados en JSON
-//			echo json_encode($rowEmp);
-
-		//	echo "<strong>".$rowEmp['Recurso_id']."</strong><br>";
-		// 	echo "Direccion: ".$rowEmp['Contenido']."<br>";
-//	   }
-//	}
-	echo 'OK';
+	$resEmp = mysql_query($queEmp, $conexion) or terminate($conexion, true, mysql_error());
+	
+	// Devolvemos el id numérico del último grupo insertado
+	$group_id = mysql_insert_id($conexion);
+	echo $group_id;
+	
+	// Insertamos al usuario en el grupo que ha creado
+	$query = "INSERT INTO grupo_usuario (grupo_id, usuario_id) VALUES(".$group_id.", ".$_SESSION['usuario_id'].")";
+	echo $query;
+	$resEmp = mysql_query($query, $conexion) or terminate($conexion, true, mysql_error());
 }
 
-mysql_close($conexion);
+terminate($conexion);
 ?>
