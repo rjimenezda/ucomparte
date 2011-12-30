94c3cf81c5cbc09d22daf1751b515136fb0dbc7a
Román Jiménez <drrouman@gmail.com>
2011-12-30 13:15:21 +0100

    Issue #5: ya se pueden crear grupos He creado un intro.php para que salga si no tienes grupos Al ir a index te redirige al primer grupo que tengas en la lista (guarro)

--- www/api/creategroup.php
+++ www/api/creategroup.php
@@ -17,12 +17,12 @@ else {
 	
 	// Devolvemos el id numérico del último grupo insertado
 	$group_id = mysql_insert_id($conexion);
-	echo $group_id;
 	
 	// Insertamos al usuario en el grupo que ha creado
 	$query = "INSERT INTO grupo_usuario (grupo_id, usuario_id) VALUES(".$group_id.", ".$_SESSION['usuario_id'].")";
-	echo $query;
 	$resEmp = mysql_query($query, $conexion) or terminate($conexion, true, mysql_error());
+	
+	echo $group_id;
 }
 
 terminate($conexion);
