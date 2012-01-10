29ee8030b97f6fee1b45b0dafc0c628c00501377
Carlos Garcia <ciyinet@gmail.com>
2012-01-10 15:26:04 +0100

    #7 Añadida imagen de presentación: Primeros pasos

--- www/you/intro.php
+++ www/you/intro.php
@@ -1,10 +1,5 @@
 <div class="content">
   	<div class="blackboard">
-	Para empezar a usar UCOmparte puedes:
-	<ul>
-	<li>Crear un grupo</li>
-	<li>Buscar y unirte a grupos que ya existan</li>
-	<li>Subir recursos</li>
-	</ul>
+	<img src="images/intro.png"></img>
    	</div>   
 </div>
